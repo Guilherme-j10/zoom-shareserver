2d7f6857f00de61c72f3e790ed1348781e9b3007
Guilherme Campos <guilhermeZA.123123@gmail.com>
2021-12-13 15:52:16 -0300

    add some commits

---
+++ dist/server.js
@@ -0,0 +1,22 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const socket_io_1 = require("socket.io");
+const http_1 = require("http");
+const app = (0, express_1.default)();
+const server = (0, http_1.createServer)(app);
+const io = new socket_io_1.Server(server);
+app.use((0, cors_1.default)());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use(express_1.default.json());
+const nameSpace = io.of('/peerConnection');
+nameSpace.on('connection', (socket) => {
+    socket.on('main_peer', (id) => {
+        socket.broadcast.emit('get_main_peer', id);
+    });
+});
+server.listen(3234);
---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import { Server } from 'socket.io';
+import { createServer } from 'http';
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+
+const nameSpace = io.of('/peerConnection');
+nameSpace.on('connection', (socket) => {
+
+  socket.on('main_peer', (id: string) => {
+    socket.broadcast.emit('get_main_peer', id);
+  });
+
+})
+
+server.listen(3234);
