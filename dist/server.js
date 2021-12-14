e6ac9a37053ef1bbd3096e093a5546fadba45163
Guilherme Campos <guilhermeZA.123123@gmail.com>
2021-12-14 09:30:20 -0300

    add peer server

--- dist/server.js
+++ dist/server.js
@@ -7,10 +7,17 @@ const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const socket_io_1 = require("socket.io");
 const http_1 = require("http");
+const peer_1 = require("peer");
+const uuid_1 = require("uuid");
 const app = (0, express_1.default)();
 const server = (0, http_1.createServer)(app);
 const io = new socket_io_1.Server(server);
+const peerServer = (0, peer_1.ExpressPeerServer)(server, {
+    path: '/peerAplication',
+    generateClientId: () => (0, uuid_1.v4)()
+});
 app.use((0, cors_1.default)());
+app.use('/', peerServer);
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use(express_1.default.json());
 const nameSpace = io.of('/peerConnection');
--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,20 @@ import express from 'express';
 import cors from 'cors';
 import { Server } from 'socket.io';
 import { createServer } from 'http';
+import { ExpressPeerServer } from 'peer';
+import { v4 } from 'uuid';
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
+const peerServer = ExpressPeerServer(server, {
+  path: '/peerAplication',
+  generateClientId: () => v4()
+});
+
 app.use(cors());
+app.use('/', peerServer);
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 
